162af85503abb315a9980ff1e6f0c3d6b1d0a485
Kyle Windsor <kwindsor@uwo.ca>
2015-01-30 21:31:20 -0500

    Updated homepage Updated styles Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # karmeter
 A meter that measures how good of a person you are via your reddit posting history.
+
+Made at UofTHacks 2015.
--- index.php
+++ index.php
@@ -4,15 +4,18 @@
 	<link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
-	<div id="intro">
+	<div id="intro" class="colorA">
 		<h1>Karmeter</h1>
-		Check the alignment of a reddit poster based on what they've posted.
+		<i>Check the alignment of a reddit poster based on what they've posted.</i>
 	</div>
 	<div id="input">
-		<!-- Input here -->
+		<form>
+			Reddit username<br>
+			<input type="text" name="username" />
+		</form>
 	</div>
-	<div id="footer">
-		Created by <a href="http://luaforfood.com" target="_blank">Kyle Windsor</a> and <a href="https://github.com/james2allen" target="_blank">James Allen</a>.
+	<div id="footer" class="colorGray">
+		Created with care by <a href="http://luaforfood.com" target="_blank">Kyle Windsor</a> and <a href="https://github.com/james2allen" target="_blank">James Allen</a>.
 	</div>
 </body>
 </html>
