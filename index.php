1e427719a80fbdd6bf5b2a228ae5562fedf83b59
Kyle Windsor <kwindsor@uwo.ca>
2015-01-31 13:51:06 -0500

    Removed redundant angular JS

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 	<title>Karmeter</title>
 	<link rel="stylesheet" href="css/style.css" />
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.11/angular.min.js"></script>
 	<script src="js/smartinput.js"></script>
 </head>
 <body>
