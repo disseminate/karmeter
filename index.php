610663a0b2578307f258ca52c09376ad31128d9f
Kyle Windsor <kwindsor@uwo.ca>
2015-01-31 14:47:43 -0500

    Added basic UI

--- index.php
+++ index.php
@@ -21,28 +21,30 @@
 		<form>
 			<input type="text" name="username" class="big clearableText" value="Reddit Username" /><input type="submit" value="Judge" class="big" />
 		</form>
-		
-		<?php
-			if( isset( $_GET["username"] ) ) {
-				$connection = new mysqli( "localhost", "root", "", "karmeter" ); // Connect to SQL
-				if( $connection->connect_errno ) { // If we couldn't connect, throw an error
-					echo( "Failed to connect to MySQL: (" . $connection->connect_errno . ") " . $connection->connect_error );
-				} else {
-					initMySQL( $connection );
-					$score = getUserScore( $connection, $_GET["username"] );
-					
-					if( $score == -2 ) {
-						echo( $_GET["username"] . " does not exist!" );
-					} else {
-						echo( $_GET["username"] . " has a score of " . $score );
-					}
-					
-					$connection->close();
-				}
-			}
-		?>
-		
 	</div>
+	<div id="bar"> </div>
+	<div id="slider"> </div>
+	<?php
+		if( isset( $_GET["username"] ) ) {
+			$connection = new mysqli( "localhost", "root", "", "karmeter" ); // Connect to SQL
+			if( $connection->connect_errno ) { // If we couldn't connect, throw an error
+				echo( "Failed to connect to MySQL: (" . $connection->connect_errno . ") " . $connection->connect_error );
+			} else {
+				initMySQL( $connection );
+				$score = getUserScore( $connection, $_GET["username"] );
+				
+				echo( '<div id="analysis">' );
+				if( $score == -2 ) {
+					echo( "<b>" . $_GET["username"] . "</b> does not exist!</div>" );
+				} else {
+					echo( "<b>" . $_GET["username"] . "</b> has a score of <b>" . round( $score * 5, 3 ) . "</b>.</div>" );
+					echo( "<div id='analysisSub'>A score of -5 is the worst and a score of 5 is the best.</div>" );
+				}
+				
+				$connection->close();
+			}
+		}
+	?>
 	<div id="footer" class="colorGray">
 		Created at UofTHacks 2015 with care by <a href="http://luaforfood.com" target="_blank">Kyle Windsor</a> and <a href="https://github.com/james2allen" target="_blank">James Allen</a>.
 	</div>
