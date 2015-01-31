787d60914d77e2aef32bdb1501a612686a279034
Kyle Windsor <kwindsor@uwo.ca>
2015-01-30 21:03:48 -0500

    Initial index

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<title>Karmeter</title>
+</head>
+<body>
+	<div id="intro">
+		<h1>Karmeter</h1>
+		Check the alignment of a reddit poster based on what they've posted.
+	</div>
+	<div id="input">
+		<!-- Input here -->
+	</div>
+	<div id="footer">
+		Created by <a href="http://luaforfood.com" target="_blank">Kyle Windsor</a> and <a href="https://github.com/james2allen" target="_blank">James Allen</a>.
+	</div>
+</body>
+</html>
