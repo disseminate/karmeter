60b164567ad5c9ebca0502fc23381b252271764d
Kyle Windsor <kwindsor@uwo.ca>
2015-01-30 23:36:07 -0500

    Added comments

--- resources/redditData.php
+++ resources/redditData.php
@@ -1,16 +1,16 @@
 <?php
 	function getUserComments( $username ) {
-		$JSON = json_decode( file_get_contents( "https://www.reddit.com/user/" . $username . ".json" ) );
+		$JSON = json_decode( file_get_contents( "https://www.reddit.com/user/" . $username . ".json" ) ); // Get the Reddit JSON
 		
 		if( isset( $JSON->error ) ) {
 			// Todo: Handle error
 		} else {
-			$releventJSON = (array)$JSON->data->children;
-			$ret = array();
+			$releventJSON = (array)$JSON->data->children; // Get the actual data from the JSON
+			$ret = array(); // Initialize return array
 			
 			for( $i = 0; $i < count( $releventJSON ); $i++ ) {
-				if( isset( $releventJSON[$i]->data->body ) ) {
-					$ret[$i] = $releventJSON[$i]->data->body;
+				if( isset( $releventJSON[$i]->data->body ) ) { // If the reddit data is from a comment, not a link/textpost
+					$ret[$i] = $releventJSON[$i]->data->body; // Add it to the return array
 				}
 			}
 			
